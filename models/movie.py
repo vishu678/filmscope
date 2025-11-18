2fb216854a28ca55b9934999700ae3a00a854f54
Vishvakumar75 <vishvakumar.dev@projecthumancity.com>
2025-11-17 21:11:41 -0500

    Added Movie domain model to represent TMDb and Firebase movie objects.

---
+++ models/movie.py
@@ -0,0 +1,7 @@
+class Movie:
+    def __init__(self, movie_id, title, rating, overview, release_date):
+        self.movie_id = movie_id
+        self.title = title
+        self.rating = rating
+        self.overview = overview
+        self.release_date = release_date
